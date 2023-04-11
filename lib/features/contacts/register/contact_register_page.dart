e4523c619b10b7421e1e7c16066a66e0a7ef8ac1
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-04-11 08:53:03 -0300

    iniciando bloc de registro

--- lib/features/contacts/register/contact_register_page.dart
+++ lib/features/contacts/register/contact_register_page.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
 
-class ContactRegisterPage extends StatelessWidget {
+class ContactRegisterPage extends StatefulWidget {
   const ContactRegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<ContactRegisterPage> createState() => _ContactRegisterPageState();
+}
+
+class _ContactRegisterPageState extends State<ContactRegisterPage> {
+  final _formkey = GlobalKey<FormState>();
+
+  final _name = TextEditingController();
+  final _email = TextEditingController();
+
+  @override
+  void dispose() {
+    _name.dispose();
+    _email.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('register')),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _name,
+                decoration: const InputDecoration(
+                  label: Text('Nome'),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'nome é obrigatório';
+                  }
+
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _email,
+                decoration: const InputDecoration(
+                  label: Text('E-mail'),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'email é obrigatório';
+                  }
+
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Salvar'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
